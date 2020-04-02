664bbc42decc6b5a78fc043bdbe323d293ff0798
KoktKorv12 <54669398+KoktKorv12@users.noreply.github.com>
2020-04-02 10:41:50 +0200

    Initial commit

---
+++ src/dev/codenmore/spel/Launcher.java
@@ -0,0 +1,11 @@
+package dev.codenmore.spel;
+
+import dev.codenmore.display.Display;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        new Display("Speltitel", 800,800);
+
+    }
+}

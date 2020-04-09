c93673f9a16a9ef273cd6a8372fc1c094faf3c87
KoktKorv12 <54669398+KoktKorv12@users.noreply.github.com>
2020-04-09 11:22:49 +0200

    Updated Spritesheet

--- src/dev/codenmore/spel/gfx/Assets.java
+++ src/dev/codenmore/spel/gfx/Assets.java
@@ -6,12 +6,17 @@ public class Assets {
 
     private static final int width = 32, height = 32;
 
-    public static BufferedImage player, dirt, grass, stone, tree;
+    public static BufferedImage dirt, grass, stone, tree;
+    public static BufferedImage[] player_down;
 
     public static void init(){
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/SPRITESHEET.png"));
 
-        player = sheet.crop(0,0, width, height);
+        player_down = new BufferedImage[2];
+
+        player_down[0] = sheet.crop();
+        player_down[1] = sheet.crop();
+
         grass = sheet.crop(width,0, width, height);
         dirt = sheet.crop(width * 2,0, width, height);
         stone = sheet.crop(width * 3,0, width, height);
